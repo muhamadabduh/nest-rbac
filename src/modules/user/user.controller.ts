817d5db845e2a32c429e33e4e670f1efd95afb6f
Muhamad Abduh <muhamad.abduh.muh@gmail.com>
2024-09-05 13:52:36 +0700

    remove nest keycloak connect middleware

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@n
 import { UserService } from './user.service'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
-import { KeycloakRolesGuard } from 'src/config/keycloak/roles.guard'
 
 @Controller('user')
 export class UserController {
