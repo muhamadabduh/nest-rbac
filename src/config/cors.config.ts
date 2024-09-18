8916af29adeeda2d60b4665a00205a3ca4de430d
Muhamad Abduh <muhamad.abduh.muh@gmail.com>
2024-09-18 10:30:51 +0700

    chore(cors): add cors helper

---
+++ src/config/cors.config.ts
@@ -0,0 +1,26 @@
+import { ForbiddenException } from '@nestjs/common'
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface'
+import { ConfigService } from '@nestjs/config'
+
+export function corsOptions(configService: ConfigService): CorsOptions {
+	const allowedCorsOrigins = configService.get('app.cors_origins')
+
+	return allowedCorsOrigins
+		? {
+				origin: (origin, callback) => {
+					// origin could be undefined or null when called directly in browser (via url, not via AJAX). This is the case on auth URL which relied on redirects. In other case, origin is guaranteed to be filled by browser so we could check the value to protect from CORS attacks
+					if (
+						!origin ||
+						origin == 'null' ||
+						allowedCorsOrigins.some((value) => origin.includes(value))
+					) {
+						callback(null, true)
+					} else {
+						callback(new ForbiddenException('CORS Error'))
+					}
+				},
+
+				credentials: true,
+		  }
+		: {}
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { corsOptions } from './config/cors.config'
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule, { cors: true })
+	const app = await NestFactory.create(AppModule)
+	const config: ConfigService = app.get(ConfigService)
+
+	app.enableCors(corsOptions(config))
 	app.useGlobalPipes(new ValidationPipe())
 	await app.listen(3000)
 }
